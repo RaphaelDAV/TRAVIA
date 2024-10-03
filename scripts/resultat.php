f851f34c03ba646878bf2feedadc9dff1704880e
RaphaelDAV <daviotraphael@gmail.com>
2024-10-03 14:53:35 +0200

    1er jet

---
+++ scripts/resultat.php
@@ -0,0 +1,25 @@
+<?php
+
+global $conn;
+require 'connexion.php';
+session_start();
+
+if (isset($_POST['submit'])) {
+    if (!empty($_POST['start']) && !empty($_POST['end'])){
+        $start = htmlspecialchars($_POST['start']);
+        $end = htmlspecialchars($_POST['end']);
+
+        $insertTravel = $conn->prepare('INSERT INTO travia.travia_trajets(depart,arrivee) VALUES(?, ?)');
+        $insertTravel->execute([$start, $end]);
+
+
+        echo "Ville de départ: $start <br>";
+        echo "Ville d'arrivée: $end";
+    }else{
+        echo "Entrez des noms de villes valides";
+    }
+} else {
+    header('location: accueil.html');
+    exit();
+}
+?>
